a870583fb6e66ec74838aa323e7d7b3d0db69ac3
worldback <39182088+worldback@users.noreply.github.com>
2024-07-12 17:14:27 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,51 @@
+import argparse
+from transformers import AutoModelForCausalLM
+import torch
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
+
+
+def get_model_and_tokenizer(model_id):
+    model = AutoPeftModelForCausalLM.from_pretrained(model_id,
+                            torch_dtype=torch.float16)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    # tokenizer.pad_token = "<pad>"
+    return model, tokenizer, device
+
+def process_input(conv: list):
+    format_data = tokenizer.apply_chat_template(conv, tokenize=False, add_generation_prompt=True)
+    print("format_data:\n", format_data)
+    return tokenizer(format_data, add_special_tokens=False)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Llama-3 model with optional fine-tuned model.")
+    parser.add_argument("--path", type=str, default="meta-llama/Llama-2-7b-chat-hf",
+                        help="The model ID or path to the fine-tuned model. Default is 'meta-llama/Llama-2-7b-hf'.")
+    args = parser.parse_args()
+    model_id = args.path
+    model, tokenizer, device = get_model_and_tokenizer(model_id)
+    system_msg = """
+    You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.
+    Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+    If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n"""
+    conv = [{"role": "system", "content": system_msg}]
+    with torch.inference_mode():
+        while True:
+            msg =input("user:").strip()
+            if msg == "q":
+                break
+            conv.append({"role": "user", "content": msg})
+            input_ids = process_input(conv)
+            if len(input_ids[0]) > 1024:
+                print("out of context window 2048, run over.")
+                break
+            output = model.generate(input_ids=input_ids.to(device), max_new_tokens=300, num_return_sequences=1)
+            generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+            prompt_length = len(tokenizer.decode(input_ids[0], skip_special_tokens=True))
+            print(f"assistant:{generated_text[prompt_length:]}")
+            conv.append({"role": "assistant", "content": generated_text[prompt_length:]})
